2248f3370bd1dce0474eb553827dcf83767edd41
Sean Fox <admin@drgunbot.com>
2023-01-24 00:59:10 +1100

    Added questions

---
+++ assets/js/questions.js
@@ -0,0 +1,42 @@
+const questions = [ 
+    {   
+        title: "What is the syntax for creating a variable in JavaScript?",        
+        choices: ["var variableName = value", "let variableName = value", "const variableName = value", "create variableName = value"],
+        answer: "var variableName = value"
+    },
+    {
+        title: "Which of the following is not a type of loop in JavaScript?",
+        choices: ["for", "while", "do-while", "repeat-until"],
+        answer: "repeat-until"
+    },
+    {
+        title: "What is the correct way to write a function in JavaScript?",
+        choices: ["function = myFunction()", "function:myFunction()", "function myFunction()", "myFunction: function()"],
+        answer: "function myFunction()"
+    },
+    {
+        title: "What is the syntax for an if statement in JavaScript?",
+        choices: ["if i == 5", "if (i == 5)", "if i = 5 then", "if i = 5"],
+        answer: "if (i == 5)"
+    },
+    {
+        title: "What is the difference between 'let' and 'var' in JavaScript?",
+        choices: ["'let' is for block-scoping and 'var' is for function scoping", "'let' is for function scoping and 'var' is for block-scoping", "There is no difference, they can be used interchangeably", "let is for variables and var for constants"],
+        answer: "'let' is for block-scoping and 'var' is for function scoping"
+    },
+    {
+        title: "What is the difference between '==' and '===' in JavaScript?",
+        choices: ["'==' checks for value equality and '===' checks for value and type equality", "'===' checks for value equality and '==' checks for value and type equality", "There is no difference, they can be used interchangeably", "== is for loose equality and === is for strict equality"],
+        answer: "'==' checks for value equality and '===' checks for value and type equality"
+    },
+    {
+        title: "What is the difference between for-loop and forEach in JavaScript?",
+        choices: ["for-loop is used for iterating through an array and forEach is used for iterating through an object", "forEach is used for iterating through an array and for-loop is used for iterating through an object", "for-loop and forEach both can be used for iterating through an array", "There is no difference, they can be used interchangeably"],
+        answer: "for-loop is used for iterating through an array and forEach is used for iterating through an array"
+    },
+    {
+        title: "What is closure in JavaScript?",
+        choices: ["A closure is an inner function that has access to the variables in the outer function's scope", "A closure is a function that returns another function", "A closure is a function that returns an object", "A closure is a function that can access global variables"],
+        answer: "A closure is an inner function that has access to the variables in the outer function's scope"
+    }
+];
